d6e6c02d96a3e2c0c2bcf0c95366206995a167de
DCloud_JSON <linju@dcloud.io>
2024-09-05 20:18:30 +0800

    更新 uni-im-msg-reader 为 1.0.2

--- uni_modules/uni-im-msg-reader/extension.js
+++ uni_modules/uni-im-msg-reader/extension.js
@@ -1,6 +1,27 @@
 import uniIm from '@/uni_modules/uni-im/sdk/index.js'
-
+import UniImMsgReader from '@/uni_modules/uni-im-msg-reader/components/uni-im-msg-reader/uni-im-msg-reader.vue'
 function install() {
+  
+  // 配置在什么情况下启用已读功能
+  uniIm.extensions.installExt('msg-extra', (msg, currentUser) => {
+    // 仅限“我”发送的消息
+    if (msg.from_uid !== uniIm.currentUser._id) return
+    // 仅限内部用户
+    if (!uniIm.currentUser.role.includes('staff')) return 
+    // 排除特殊消息
+    if (msg.type == 'system' || msg.is_revoke) return
+    // 仅限私聊会话
+    let conversation = uniIm.conversation.find(msg.conversation_id)
+    if (!conversation?.friend_uid) return
+    return {
+      component: UniImMsgReader,
+      props: {
+        msg
+      }
+    }
+  })
+  
+  
   // 注册 read_msg 消息类型
   uniIm.extensions.installExt('msg-type-register', () => {
     return {
@@ -18,38 +39,39 @@ function install() {
           from_uid,
           create_time,
         } = msgData
-        const msg = conversation.msgList.find(msg => msg._id === msgId)
+        // debugger
+        const msg = conversation.msg.find(msgId)
         if (msg) {
           let reader = {
             user_id: from_uid,
             create_time
           }
           msg.reader_list ? msg.reader_list.push(reader) : msg.reader_list = [reader]
-          conversation.msgManager.localMsg.update(msg.unique_id, msg)
         }
       },
     }
   })
 
   // 监听每条消息的显示状态，报送 read_msg 消息
-  uniIm.extensions.installExt('msg-appear', (msg, currentUser) => {
+  uniIm.extensions.installExt('msg-appear', msg => {
+    const currentUid = uniIm.currentUser._id
     // 特殊消息不用处理
     if (msg.type === 'system' || msg.is_revoke) return
 
     // 如果是我发送的消息则不处理
-    if (msg.from_uid == currentUser.user_id) return
+    if (msg.from_uid == currentUid) return
 
     // 如果我已经在已读列表里则不再处理
-    if (msg.reader_list?.some(u => u.user_id == currentUser.user_id)) return
+    if (msg.reader_list?.some(u => u.user_id == currentUid)) return
 
     // 如果是群聊消息且没有 @我，则不用处理
-    let conversation = uniIm.conversation.getCached(msg.conversation_id)
-    if (conversation.group_id && !msg.call_uid?.includes(currentUser.user_id)) return
+    let conversation = uniIm.conversation.find(msg.conversation_id)
+    if (conversation.group_id && !msg.call_uid?.includes(currentUid)) return
 
     // 把自己记入已读列表
     msg.reader_list = msg.reader_list || []
     msg.reader_list.push({
-      user_id: currentUser.user_id,
+      user_id: currentUid,
       create_time: Date.now()
     })
 
@@ -64,7 +86,7 @@ function install() {
       }
     }).catch(e => {
       // 提交失败，把自己从已读列表里去掉
-      msg.reader_list = msg.reader_list.filter(u => u.user_id !== currentUser.user_id)
+      msg.reader_list = msg.reader_list.filter(u => u.user_id !== currentUid)
     })
   })
 }
